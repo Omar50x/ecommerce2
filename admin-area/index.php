e366170912940917daa26baf0236bc0c8bf19fef
Omar Zekri <102541219+Omar50x@users.noreply.github.com>
2022-10-09 22:35:12 +0100

    Insert Brands

--- admin-area/index.php
+++ admin-area/index.php
@@ -7,8 +7,20 @@
     <title>Admin dashboard</title>
     <!-- Bootstrap CSS link -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <!-- Font awesome link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <!-- CSS file -->
     <link rel="stylesheet" href="../style.css">
+    <style>
+        .admin_image{
+            width: 100px;
+            object-fit: contain;
+        }
+        .footer{
+            position: absolute;
+            bottom: 0;
+        }
+    </style>
 </head>
 <body>
     <!-- Navbar -->
@@ -28,7 +40,48 @@
         </div>
 
         <!-- Second Child -->
-        <div class="bg-light"></div>
+        <div class="bg-light">
+            <h3 class="text-center p-2">Manage Details</h3>
+        </div>
+
+        <!-- Third Child -->
+        <div class="row">
+            <div class="col-md-12 bg-secondary p-1 d-flex align-items-center">
+                <div class="p-3">
+                    <a href="#"><img src="../images/pineapple-drink-500x500.jpg" alt="" class="admin_image"></a>
+                    <p class="text-light text-center">Admin Name</p>
+                </div>
+                <div class="button text-center">
+                    <button class="my-3"><a href="" class="nav-link text-light bg-info my-1">Insert Products</a></button>
+                    <button><a href="" class="nav-link text-light bg-info my-1">View Products</a></button>
+                    <button><a href="index.php?insert_category" class="nav-link text-light bg-info my-1">Insert Categories</a></button>
+                    <button><a href="" class="nav-link text-light bg-info my-1">View Categories</a></button>
+                    <button><a href="index.php?insert_brand" class="nav-link text-light bg-info my-1">Insert Brands</a></button>
+                    <button><a href="" class="nav-link text-light bg-info my-1">View Brands</a></button>
+                    <button><a href="" class="nav-link text-light bg-info my-1">All Orders</a></button>
+                    <button><a href="" class="nav-link text-light bg-info my-1">All Payments</a></button>
+                    <button><a href="" class="nav-link text-light bg-info my-1">List Users</a></button>
+                    <button><a href="" class="nav-link text-light bg-info my-1">Logout</a></button>
+                </div>
+            </div>
+        </div>
+
+        <!-- Fourth Child -->
+        <div class="container my-3">
+            <?php
+                if (isset($_GET['insert_category'])) {
+                    include('insert_categories.php');
+                }
+                if (isset($_GET['insert_brand'])) {
+                    include('insert_brands.php');
+                }
+            ?>
+        </div>
+
+        <!-- Last Child -->
+        <div class="bg-info p-3 text-center footer">
+            <p>All rights reserved &copy; Designed by Omar-2022</p>
+        </div>
     </div>
 
     <!-- Bootstrap Js link -->
--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+    include('includes/connect.php');
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -148,21 +152,19 @@
                     <li class="nav-item bg-info">
                         <a href="#" class="nav-link text-light"><h4>Delivery Brands</h4></a>
                     </li>
-                    <li class="nav-item">
-                        <a href="#" class="nav-link text-light">Brand1</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#" class="nav-link text-light">Brand2</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#" class="nav-link text-light">Brand3</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#" class="nav-link text-light">Brand4</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#" class="nav-link text-light">Brand4</a>
-                    </li>
+                    <?php
+                        $select_brands = "select * from `brands`";
+                        $result_brands = mysqli_query($con, $select_brands);
+                        while ($row_data = mysqli_fetch_assoc($result_brands)) {
+                            $brand_title = $row_data['brand_title'];
+                            $brand_id = $row_data['brand_id'];
+                            echo "
+                            <li class='nav-item'>
+                                <a href='index.php?brand=$brand_id' class='nav-link text-light'>$brand_title</a>
+                            </li>
+                            ";
+                        }
+                    ?>
                 </ul>
 
                 <!-- Categories to be displayed -->
@@ -170,30 +172,28 @@
                     <li class="nav-item bg-info">
                         <a href="#" class="nav-link text-light"><h4>Categories</h4></a>
                     </li>
-                    <li class="nav-item">
-                        <a href="#" class="nav-link text-light">Categorie1</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#" class="nav-link text-light">Categorie2</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#" class="nav-link text-light">Categorie3</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#" class="nav-link text-light">Categorie4</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#" class="nav-link text-light">Categorie4</a>
-                    </li>
+                    <?php
+                        $select_categories = "select * from `categories`";
+                        $result_categories = mysqli_query($con, $select_categories);
+                        while ($row_data = mysqli_fetch_assoc($result_categories)) {
+                            $category_title = $row_data['category_title'];
+                            $category_id = $row_data['category_id'];
+                            echo "
+                            <li class='nav-item'>
+                                <a href='index.php?category=$category_id' class='nav-link text-light'>$category_title</a>
+                            </li>
+                            ";
+                        }
+                    ?>
                 </ul>
             </div>
 
         </div>
 
         <!-- Last Child -->
-        <!-- <div class="bg-info p-3 text-center">
+        <div class="bg-info p-3 text-center">
             <p>All rights reserved &copy; Designed by Omar-2022</p>
-        </div> -->
+        </div>
     </div>
 
     <!-- Bootstrap Js link -->
